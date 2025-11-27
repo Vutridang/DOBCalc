29bb257f46bc52068d2f9f9c1b63761324510f7f
Vutridang <dangvu8244@gmail.com>
2025-11-27 16:08:37 +0700

    Add click listener with Toast for btnDatePicker button

--- app/src/main/java/eu/tutorials/dobcalc/MainActivity.kt
+++ app/src/main/java/eu/tutorials/dobcalc/MainActivity.kt
@@ -1,16 +1,21 @@
 package eu.tutorials.dobcalc
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+
+        btnDatePicker.setOnClickListener {
+            Toast.makeText(this, "btnDatePicker press", Toast.LENGTH_LONG).show()
+        }
     }
+
+
 }
