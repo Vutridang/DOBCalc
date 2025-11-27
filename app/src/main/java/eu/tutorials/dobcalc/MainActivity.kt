837de62d596952037ca14c669d38adc72355e1c3
Vutridang <dangvu8244@gmail.com>
2025-11-27 16:24:36 +0700

    feat: add clickDatePicker function using DatePickerDialog lambda

--- app/src/main/java/eu/tutorials/dobcalc/MainActivity.kt
+++ app/src/main/java/eu/tutorials/dobcalc/MainActivity.kt
@@ -1,21 +1,38 @@
 package eu.tutorials.dobcalc
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+        val btnDatePicker: Button = findViewById(R.id.btnDatePicker)
 
         btnDatePicker.setOnClickListener {
-            Toast.makeText(this, "btnDatePicker press", Toast.LENGTH_LONG).show()
+            clickDatePicker()
         }
     }
 
+    fun clickDatePicker() {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
 
+        DatePickerDialog(this,
+            { view, year, month, dayOfMont ->
+                Toast.makeText(this, "Datepicker works", Toast.LENGTH_LONG).show()
+            },
+            year,
+            month,
+            day
+        ).show()
+    }
 }
